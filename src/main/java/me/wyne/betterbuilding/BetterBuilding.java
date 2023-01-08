6a38913e79d120b3aca82b75ff618ecd33b7c8c9
Wyne <officialfitcher@gmail.com>
2023-01-08 19:18:23 +0500

    Init commit

---
+++ src/main/java/me/wyne/betterbuilding/BetterBuilding.java
@@ -0,0 +1,10 @@
+package me.wyne.betterbuilding;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterBuilding implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
